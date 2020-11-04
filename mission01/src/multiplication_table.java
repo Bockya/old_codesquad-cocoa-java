d2a7779fecf971573cc5dcdf54fa26f1af1f6a35
Bockya <rhk3562@naver.com>
2020-11-04 16:53:51 +0900

    fix method

--- mission01/src/multiplication_table.java
+++ mission01/src/multiplication_table.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 public class multiplication_table {
-    public static void main(String[] args) {
+    public int start;
+    public int end;
+    public void init(){
         Scanner sc = new Scanner(System.in); //system은 클래스
-        //Scanner sc = new Scanner("hello.txt");
         System.out.println("시작단을 입력하세요: "); //out: static 개체, println: 메소드
-        int start = sc.nextInt();
+        this.start = sc.nextInt();
         System.out.println("끝단을 입력하세요: ");
-        int end = sc.nextInt();
+        this.end = sc.nextInt();
+        sc.close(); //마지막에 꼭 닫기
+    }
+    public void print(){
         for (int j = 1; j <= 9; j++) {
-            for (int i = start; i <= end; i++)
+            for (int i = this.start; i <= this.end; i++)
                 System.out.print(i + "*" + j + "=" + i * j + "\t");
             System.out.println();
         }
-        sc.close(); //마지막에 꼭 닫기
+    }
+    public static void main(String[] args) {
+        multiplication_table mt=new multiplication_table();
+        mt.init();
+        mt.print();
     }
 }
 //주석은 짧을 수록 좋다
+//Scanner sc = new Scanner("hello.txt");
