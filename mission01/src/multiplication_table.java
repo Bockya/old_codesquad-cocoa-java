c17fee96d3a495c6231a4fe5e4a362b101cdc0d8
Bockya <rhk3562@naver.com>
2020-11-06 11:08:09 +0900

    fix private, add valid

--- mission01/src/multiplication_table.java
+++ mission01/src/multiplication_table.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 public class multiplication_table {
-    public int start;
-    public int end;
-    public void init(){
+    private int start;
+    private int end;
+    private boolean getInput(){
         Scanner sc = new Scanner(System.in); //system은 클래스
-        System.out.println("시작단을 입력하세요: "); //out: static 개체, println: 메소드
-        this.start = sc.nextInt();
-        System.out.println("끝단을 입력하세요: ");
-        this.end = sc.nextInt();
+        System.out.println("시작단과 끝단을 입력하세요: "); //out: static 개체, println: 메소드
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        if(start < 1 || end > 9 || start > end){
+            System.out.println("범위는 1에서 9까지 입니다.");
+            return false;
+        }
+        this.start=start;
+        this.end=end;
         sc.close(); //마지막에 꼭 닫기
+        return true;
     }
-    public void print(){
+    private void print(){
         for (int j = 1; j <= 9; j++) {
             for (int i = this.start; i <= this.end; i++)
                 System.out.print(i + "*" + j + "=" + i * j + "\t");
@@ -19,7 +25,9 @@ public class multiplication_table {
     }
     public static void main(String[] args) {
         multiplication_table mt=new multiplication_table();
-        mt.init();
+        boolean valid =mt.getInput();
+        while (!valid)
+            valid=mt.getInput();
         mt.print();
     }
 }
