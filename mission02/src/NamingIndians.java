3539bdaa2df829dce652c97e8ada96b6f818bfa1
Bockya <rhk3562@naver.com>
2020-11-06 12:24:49 +0900

    fix

--- mission02/src/NamingIndians.java
+++ mission02/src/NamingIndians.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class NamingIndians {
-    public Scanner sc=new Scanner(System.in);
-    public void indianname(int year, int month, int day){
+    private Scanner sc=new Scanner(System.in);
+    private void indianname(int year, int month, int day){
         String[] indianYear={"말많은","푸른","적색","조용한","웅크린","백색","지혜로운","용감한"," 날카로운","욕심많은"};
         String[] indianMonth={"늑대","태양","양","매","황소","불꽃","나무","달빛","말","돼지","하늘","바람"};
         String[] indianDay={"와(과) 함께 춤을","의 기상","은(는) 그림자 속에","없습니다","없습니다","없습니다","의 환생","의 죽음","아래에서","(을) 보라",
@@ -9,17 +9,17 @@ public class NamingIndians {
                 "의 고향","의 전사","은(는) 나의친구","의 노래","의 정령","의 파수꾼","의 악마","와(과)같은 사나이","를(을) 쓰러트린자","의 혼","은(는) 말이 없다"};
         System.out.println("당신의 이름은 "+indianYear[year]+' '+indianMonth[month]+indianDay[day]+"입니다.");
     }
-    public int getyear(){
+    private int getyear(){
         int year=this.sc.nextInt();
         year=year%10;
         return year;
     }
-    public int getmonth(){
+    private int getmonth(){
         int month=this.sc.nextInt();
         month--;
         return month;
     }
-    public int getday(){
+    private int getday(){
         int day=this.sc.nextInt();
         day--;
         return day;
