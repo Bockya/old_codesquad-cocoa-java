13cf7a64fee109a7c88db1a25e94e215a2e7f638
Bockya <rhk3562@naver.com>
2020-11-16 18:04:05 +0900

    commit2

--- mission5/src/HangeulClock.java
+++ mission5/src/HangeulClock.java
@@ -6,88 +6,175 @@ public class HangeulClock {
         switch (hour) {
             case 1:
             case 13:
-                String[] hanHour1 = {"한□□□□□",
+                String[] h1 = {"한□□□□□",
                         "□□□□□□",
                         "□□□□□시"};
-                result = hanHour1;
+                result = h1;
                 break;
             case 2:
             case 14:
-                String[] hanHour2 = {"□두□□□□",
+                String[] h2 = {"□두□□□□",
                         "□□□□□□",
                         "□□□□□시"};
-                result = hanHour2;
+                result = h2;
                 break;
             case 3:
             case 15:
-                String[] hanHour3 = {"□□세□□□",
+                String[] h3 = {"□□세□□□",
                         "□□□□□□",
                         "□□□□□시"};
-                result = hanHour3;
+                result = h3;
                 break;
             case 4:
             case 16:
-                String[] hanHour4 = {"□□□네□□",
+                String[] h4 = {"□□□네□□",
                         "□□□□□□",
                         "□□□□□시"};
-                result = hanHour4;
+                result = h4;
                 break;
             case 5:
             case 17:
-                String[] hanHour5 = {"□□□□다섯",
+                String[] h5 = {"□□□□다섯",
                         "□□□□□□",
                         "□□□□□시"};
-                result = hanHour5;
+                result = h5;
                 break;
             case 6:
             case 18:
-                String[] hanHour6 = {"□□□□□□",
+                String[] h6 = {"□□□□□□",
                         "여섯□□□□",
                         "□□□□□시"};
-                result = hanHour6;
+                result = h6;
                 break;
             case 7:
             case 19:
-                String[] hanHour7 = {"□□□□□□",
+                String[] h7 = {"□□□□□□",
                         "□□일곱□□",
                         "□□□□□시"};
-                result = hanHour7;
+                result = h7;
                 break;
             case 8:
             case 20:
-                String[] hanHour8 = {"□□□□□□",
+                String[] h8 = {"□□□□□□",
                         "□□□□여덟",
                         "□□□□□시"};
-                result = hanHour8;
+                result = h8;
                 break;
             case 9:
             case 21:
-                String[] hanHour9 = {"□□□□□□",
+                String[] h9 = {"□□□□□□",
                         "□□□□□□",
                         "아홉□□□시"};
-                result = hanHour9;
+                result = h9;
                 break;
             case 10:
             case 22:
-                String[] hanHour10 = {"□□□□□□",
+                String[] h10 = {"□□□□□□",
                         "□□□□□□",
                         "□□열□□시"};
-                result = hanHour10;
+                result = h10;
                 break;
             case 11:
             case 23:
-                String[] hanHour11 = {"□□□□□□",
+                String[] h11 = {"□□□□□□",
                         "□□□□□□",
                         "□□열한□시"};
-                result = hanHour11;
+                result = h11;
                 break;
             case 12:
             case 24:
-                String[] hanHour12 = {"□□□□□□",
+                String[] h12 = {"□□□□□□",
                         "□□□□□□",
                         "□□열□두시"};
-                result = hanHour12;
+                result = h12;
                 break;
+        }
+        return result;
+    }
+
+    private String getHanTenMin(int minute) {
+        String result="";
+        switch (minute / 10) {
+            case 1:
+                String tm1 = "□□□□□십";
+                result = tm1;
+                break;
+            case 2:
+                String tm2 = "□이□□□십";
+                result = tm2;
+                break;
+            case 3:
+                String tm3 = "□□삼□□십";
+                result = tm3;
+                break;
+            case 4:
+                String tm4 = "□□□사□십";
+                result = tm4;
+                break;
+            case 5:
+                String tm5 = "□□□□오십";
+                result = tm5;
+                break;
+            case 0:
+                String tm0 = "□□□□□□";
+                result = tm0;
+                break;
+        }
+        return result;
+    }
+
+    private String[] getHanOneMin(int minute) {
+        String[] result = new String[2];
+        switch (minute % 10) {
+            case 0:
+                String[] om0 = {"□□□□□□",
+                        "□□□□□분"};
+                result = om0;
+                break;
+            case 1:
+                String[] om1 = {"□일□□□□",
+                        "□□□□□분"};
+                result = om1;
+                break;
+            case 2:
+                String[] om2 = {"□□이□□□",
+                        "□□□□□분"};
+                result = om2;
+                break;
+            case 3:
+                String[] om3 = {"□□□삼□□",
+                        "□□□□□분"};
+                result = om3;
+                break;
+            case 4:
+                String[] om4 = {"□□□□사□",
+                        "□□□□□분"};
+                result = om4;
+                break;
+            case 5:
+                String[] om5 = {"□□□□□□",
+                        "□오□□□분"};
+                result = om5;
+                break;
+            case 6:
+                String[] om6 = {"□□□□□□육",
+                        "□□□□□□분"};
+                result = om6;
+                break;
+            case 7:
+                String[] om7 = {"□□□□□□",
+                        "□□칠□□분"};
+                result = om7;
+                break;
+            case 8:
+                String[] om8 = {"□□□□□□",
+                        "□□□팔□분"};
+                result = om8;
+                break;
+            case 9:
+                String[] om9 = {"□□□□□□",
+                        "□□□□구분"};
+                result = om9;
         }
         return result;
     }
@@ -110,22 +197,26 @@ public class HangeulClock {
         String[] hanHour = {"한두세네다섯",
                 "여섯일곱여덟",
                 "아홉열한두시"};
-        String[] hanMinute = {"자이삼사오십",
-                "정일이삼사육",
+        String hanTenMinute = "자이삼사오십";
+        String[] hanOneMinute = {"정일이삼사육",
                 "오오칠팔구분"};
-        String[] result = hc.getHanHour(hour);
-
+        String[] result1 = hc.getHanHour(hour);
+        String result2 = hc.getHanTenMin(minute);
+        String[] result3 = hc.getHanOneMin(minute);
 
         System.out.println("now: " + now);
         System.out.println("hour: " + hour);
         System.out.println("minute: " + minute);
-        System.out.println(si);
-        System.out.println(bun);
-        System.out.println(jajeong);
-        System.out.println(jeongo);
-        for (int i = 0; i < 6; i++)
-            System.out.println(hangeul[i]);
-        for(int i=0;i<3;i++)
-            System.out.println(result[i]);
+        //System.out.println(si);
+        //System.out.println(bun);
+        //System.out.println(jajeong);
+        //System.out.println(jeongo);
+        //for (int i = 0; i < 6; i++)
+        //    System.out.println(hangeul[i]);
+        for (int i = 0; i < 3; i++)
+            System.out.println(result1[i]);
+        System.out.println(result2);
+        for (int i = 0; i < 2; i++)
+            System.out.println(result3[i]);
     }
 }
